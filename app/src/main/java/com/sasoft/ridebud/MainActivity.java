50da0fb9a3badacd7d3ce4995eeab85257a63472
SeffanaAlliera <markaleson@gmail.com>
2023-06-10 14:57:49 +0800

    initial commit

---
+++ app/src/main/java/com/sasoft/ridebud/MainActivity.java
@@ -0,0 +1,71 @@
+package com.sasoft.ridebud;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle menu item clicks here
+        int itemId = menuItem.getItemId();
+
+        if (itemId == R.id.nav_item1) {
+            // Handle Item 1 click
+            Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.nav_item2) {
+            // Handle Item 2 click
+            Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show();
+        }
+
+        // Close the drawer after handling the click
+        drawerLayout.closeDrawers();
+
+        return true;
+    }
+
+}
